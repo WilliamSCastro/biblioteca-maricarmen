80aef74b12560180210db97916d630dd949e7641
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-04-07 18:32:38 +0200

    Spec 7: -Añadida vista Exemplars al admin panel de Bibliotecari -->Ahora mismo pre-selecciona el centre del usuario logueado, hay un código comentado que guarda automáticamente el centre del usuario independientemente de lo que seleccione. Preguntar a Enric cómo lo quiere. Spec 8: -Añadida vista Usuaris al admin panel de Bibliotecari --> Añadido campo de búsqueda de usuaris a su vista para agilizar tareas, puede buscar por username, nombre y/o apellidos --> El Bibliotecari únicamente puede modificar el teléfono de los usuarios. El resto de campos están en sólo lectura. Preguntar a Enric si es lo deseado.

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -10,13 +10,13 @@ class CategoriaAdmin(admin.ModelAdmin):
 	ordering = ('parent','nom')
 
 
-class UsuariAdmin(UserAdmin):
-    # Keep your existing fieldsets
-    fieldsets = UserAdmin.fieldsets + (
-            ("Dades addicionals", { # Changed title slightly for clarity
-                'fields': ('centre', 'cicle', 'telefon', 'imatge'),
-            }),
-    )
+# class UsuariAdmin(UserAdmin):
+#     # Keep your existing fieldsets
+#     fieldsets = UserAdmin.fieldsets + (
+#             ("Dades addicionals", { # Changed title slightly for clarity
+#                 'fields': ('centre', 'cicle', 'telefon', 'imatge'),
+#             }),
+#     )
 
     # # Add custom fields to list display for easier viewing/sorting
     # list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'centre', 'cicle', 'telefon')
@@ -75,6 +75,68 @@ class UsuariAdmin(UserAdmin):
     #     # For now, let's assume they can set the phone number on creation if they have add_usuari perm
 
     #     return tuple(readonly_fields)
+	
+# vistas para el usuario Bibliotecari: 
+# debe poder registrar nuevos Exemplars
+
+class ExemplarAdmin(admin.ModelAdmin):
+    #bloque que aplica permisos de admin panel a usuarios staff
+    def staff_permission(self, request):
+        return request.user.is_staff
+
+    def has_module_permission(self, request):
+        return self.staff_permission(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self.staff_permission(request)
+
+    def has_view_permission(self, request, obj=None):
+        return self.staff_permission(request)
+
+    def has_add_permission(self, request):
+        return self.staff_permission(request)
+    
+    # # si queremos sobreescribir el valor centre por el del usuario
+    # def save_model(self, request, obj, form, change):
+    #     if not obj.pk:
+    #         obj.centre = request.user.centre
+    #     super().save_model(request, obj, form, change)
+
+    # # si queremos que se muestre el valor centre por el del usuario
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['centre'].initial = request.user.centre
+        return form
+    
+class UsuariAdmin(admin.ModelAdmin):
+    search_fields = ['username__icontains', 'first_name__icontains', 'last_name__icontains']
+
+    #bloque que aplica permisos de admin panel a usuarios staff
+    def staff_permission(self, request):
+        return request.user.is_staff
+
+    def has_module_permission(self, request):
+        return self.staff_permission(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self.staff_permission(request)
+
+    def has_view_permission(self, request, obj=None):
+        return self.staff_permission(request)
+    
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_staff and not request.user.is_superuser:
+            return ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'date_joined', 'last_login']
+        return []
+
+    def get_fields(self, request, obj=None):
+        if request.user.is_staff and not request.user.is_superuser:
+            return ['telefon'] + self.get_readonly_fields(request, obj)
+        return ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'date_joined', 'last_login']
+    
+
+
+    
 
 class ExemplarsInline(admin.TabularInline):
 	model = Exemplar
@@ -94,6 +156,7 @@ class LlibreAdmin(admin.ModelAdmin):
 		return mark_safe("<img src='{}' />".format(escape(obj.thumbnail_url)))
 	thumb.allow_tags = True
 
+admin.site.register(Exemplar, ExemplarAdmin)
 admin.site.register(Usuari,UsuariAdmin)
 admin.site.register(Categoria,CategoriaAdmin)
 admin.site.register(Pais)
